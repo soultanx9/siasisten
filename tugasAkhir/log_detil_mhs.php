d062a079445a2e5e91251b55884fd9960dc66a25
agungak <agungahmadk@gmail.com>
2016-12-03 12:44:52 +0700

    Add files via upload

--- tugasAkhir/log_detil_mhs.php
+++ tugasAkhir/log_detil_mhs.php
@@ -5,13 +5,75 @@
 	<link rel="stylesheet" href="css/lihat-log.css">
 	<script src="javascript/jquery.min.js"></script>
 	<script src="javascript/bootstrap.js"></script>
-	
+	<script>
+		function validateForm() {
+			var kategori = document.forms["myForm"]["kategori"].value;
+			var tanggal = document.forms["myForm"]["tanggal"].value;
+			var jam_mulai = document.forms["myForm"]["jam_mulai"].value;
+			var jam_selesai = document.forms["myForm"]["jam_selesai"].value;
+			var deskripsi_kerja = document.forms["myForm"]["deskripsi_kerja"].value;
+			if (kategori == "") {
+				alert("kategori harus dipilih");
+				return false;
+			}if (tanggal == "") {
+				alert("tanggal harus diisi");
+				return false;
+			}if (jam_mulai == "") {
+				alert("jam mulai harus diisi");
+				return false;
+			}if (jam_selesai == "") {
+				alert("jam selesai harus diisi");
+				return false;
+			}if (deskripsi_kerja == "") {
+				alert("deskripsi kerja harus diisi");
+				return false;
+			}
+			if(jam_mulai >= jam_selesai){
+				alert("jam salah, jam mulai harus lebih dahulu");
+				return false;
+			}		
+		}
+		</script>
 	<meta charset="UTF-8">
 	<meta name="description" content="LOG">
 
 <title>LOG</title>
 </head>
 <body>
+	<?php
+	$tmperror="";
+	if(isset($_GET["kategori"])){
+			$kategori = substr($_GET["kategori"],0,1);
+			$tanggal = $_GET["tanggal"];
+			$jam_mulai = $tanggal." ".$_GET["jam_mulai"];
+			$jam_selesai = $tanggal." ".$_GET["jam_selesai"];
+			$deskripsi_kerja = $_GET["deskripsi_kerja"];
+			$npm =	"1127242324";		
+			$default = 1;
+			$conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=basdat") or die('connection failed');
+			$result = pg_query($conn, "select max(id) from log");
+			$id;		
+			while($tmp = pg_fetch_array($result)){
+				$id = $tmp[0]+1;
+			}
+			$sql = "INSERT into log (id, idlamaran, npm, id_kat_log, id_st_log, tanggal, jam_mulai, jam_selesai, deskripsi_kerja) 
+			VALUES('$id',113,'$npm','$kategori','$default',to_timestamp('$tanggal','YYYY/MM/DD'),to_timestamp('$jam_mulai','YYYY/MM/DD hh24:mi'),to_timestamp('$jam_selesai','YYYY/MM/DD hh24:mi'), '$deskripsi_kerja')";
+			
+						
+			if(pg_query($conn, $sql)){
+			$tmperror = "<div class=\"alert alert-success fade in\">
+								<a href=\"#\" class=\"close\" data-dismiss\=\"alert\">&times;</a>Penyimpanan berhasil
+										
+							</div>";
+			}else{
+				$tmperror = "something wrong";
+			}
+		}
+	
+	
+	?>
+
+
 
 <div class="jumbotron text-center">
   <h1><b>Daftar Log</b></h1>
@@ -19,7 +81,7 @@
 
 </div>
 <div class="container">
-
+	<?php echo $tmperror;?>
 		
   <div class="row">
   <div class="col-md-12">
@@ -61,12 +123,11 @@
 										"<td>".$log[3]."</td>".
 										"<td>".$log[4]."</td>".
 										(($log[5] == "dilaporkan")?"<td>-</td>":"<td>".$log[5]."</td>").
-										(($log[5]!="disetujui")?"<td class=\"text-center\"><button class=\"btn btn-primary btn-xs\" data-title=\"Edit\" data-toggle=\"modal\" data-target=\"#edit\" >
+										(($log[5]!="dilaporkan")?"<td></td>":"<td class=\"text-center\"><button class=\"btn btn-primary btn-xs\" data-title=\"Edit\" data-toggle=\"modal\" data-target=\"#edit\" >
 										<span class=\"glyphicon glyphicon-pencil\"></span></button>
 								
 										<button class=\"btn btn-danger btn-xs\" data-title=\"Delete\" data-toggle=\"modal\" data-target=\"#delete\" >
-										<span class=\"glyphicon glyphicon-remove\"></span></button>	</td>"
-										:"<td></td>")."</tr>";
+										<span class=\"glyphicon glyphicon-remove\"></span></button>	</td>")."</tr>";
 										
 										
 									   
@@ -74,49 +135,22 @@
 								}
 	
 	?>
-	
-	
-	
-      <tr>
-		<td>Mengoreksi</td>
-        <td>12-09-2016</td>
-        <td>09:00</td>
-		<td>11:00</td>
-		<td>Tugas 2</td>
-		<td>-</td>
-		<td class="text-center"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" >
-		<span class="glyphicon glyphicon-pencil"></span></button>
-		
-        <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" >
-		<span class="glyphicon glyphicon-remove"></span></button>
-		</td>
-      </tr>
-      <tr class="success">
-       <td>mengawas</td>
-        <td>12-09-2016</td>
-        <td>10:00</td>
-		<td>11:00</td>
-		<td>UTS</td>
-		<td>Disetujui</td>
-		<td></td>
-      </tr>
-	  <tr>
-        <td>Mengoreksi</td>
-        <td>10-09-2016</td>
-        <td>10:00</td>
-		<td>12:00</td>
-		<td>Tugas 1</td>
-		<td>Ditolak</td>
-		<td></td>
-      </tr>
-    </tbody>
+	</tbody>
   </table>
     </div>
+	
+	
+	
+	
+	
+	
 	
 	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="vertical-alignment-helper">
         <div class="modal-dialog vertical-align-center">
             <div class="modal-content">
+				<form name="myForm" action="log_detil_mhs.php" onsubmit="return validateForm()" method="get">
+					
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
 
@@ -125,34 +159,37 @@
 
                 </div>
                 <div class="modal-body">
-						
+
 					  <div class="table-responsive">
 					  <table class="table table-bordered">
 					  <tr>
 						<th class="col-md-4">Kategori</th>
-						<td><select class="form-control">
-						  <option>1</option>
-						  <option>2</option>
-						  <option>3</option>
-						  <option>4</option>
-						  <option>5</option>
+						<td><select class="form-control" id = "kategori" name = "kategori">
+						<option></option>
+						<?php
+						$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=basdat") or die('connection failed');
+						$result = pg_query($dbconn, "select * from kategori_log;");
+						while($kategori = pg_fetch_array($result)){
+									echo "<option>".$kategori[0]." - ".$kategori[1]."</option>";
+						}
+						?>
 						</select></td>
 					  </tr>
 					  <tr>
 						<th>Tanggal</th>
-						<td><input type="date" class="form-control" id="usr"></td>
+						<td><input type="date" class="form-control" id="tanggal" name="tanggal"></td>
 					  </tr>
 					  <tr>
 						<th>Jam Mulai</th>
-						<td><input type="time" class="form-control" id="usr"></td>
+						<td><input type="time" class="form-control" id="jam_mulai" name="jam_mulai" ></td>
 					  </tr>
 					   <tr>
 						<th>Jam Selesai</th>
-						<td><input type="time" class="form-control" id="usr"></td>
+						<td><input type="time" class="form-control" id="jam_selesai" name="jam_selesai"></td>
 					  </tr>
 					   <tr>
 						<th>Deskripsi Kerja</th>
-						<td><textarea class="form-control" rows="3"></textarea></td>
+						<td><textarea class="form-control" rows="3" id ="deskripsi_kerja" name="deskripsi_kerja"></textarea></td>
 					  </tr>
 					  </table>
 					</div>
@@ -161,9 +198,10 @@
 				
 				</div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                    <input type="submit" value="submit" class="btn btn-primary" id="submit">
                 </div>
+			</form>	
             </div>
         </div>
     </div>
